9565ee168c9b230194e91a150e94f14b39b50cd8
noamsito <vieiranoam94@gmail.com>
2025-03-12 15:01:14 -0300

    setup files

---
+++ Proyecto-Megaten-IIC2113/Shin-Megami-Tensei-Controller/Game.cs
@@ -0,0 +1,15 @@
+﻿using Shin_Megami_Tensei_View;
+
+namespace Shin_Megami_Tensei;
+
+public class Game
+{
+    public Game(View view, string teamsFolder)
+    {
+    }
+    
+    public void Play()
+    {
+        throw new NotImplementedException();
+    }
+}
